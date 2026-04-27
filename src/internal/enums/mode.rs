ee1ebfa6468cd2659c9f146623525aa97518e6eb
prranavv <pranavvkumar03@gmail.com>
2026-04-27 11:04:42 +0530

    implemented the From trait on Mode

--- src/internal/enums/mode.rs
+++ src/internal/enums/mode.rs
@@ -6,3 +6,13 @@ pub enum Mode{
     Executable = 0o100755,
     Directory = 0o040000
 }
+
+impl From<&str> for Mode{
+    fn from(value: &str) -> Self {
+        match value{
+            "100644" => return Mode::File,
+            "40000" => return Mode::Directory,
+            _=> return Mode::Executable
+        }
+    }
+}
