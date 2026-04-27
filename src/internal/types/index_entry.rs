e582255ce4fc0f112e0a19b349c3bb256ad17390
prranavv <pranavvkumar03@gmail.com>
2026-04-27 12:11:49 +0530

    implemented From<IndexEntry> for String

--- src/internal/types/index_entry.rs
+++ src/internal/types/index_entry.rs
@@ -1,5 +1,6 @@
 use crate::internal::Mode;
 
+#[derive(Debug)]
 pub struct IndexEntry{
     pub mode: Mode,
     pub file_path: String,
@@ -11,10 +12,11 @@ impl IndexEntry{
     pub fn new(mode:Mode,file_path:String,hash:String)->Self{
         Self { mode, file_path, hash }
     }
+}
 
-    pub fn parse(raw_entry:String)->Self{
-        // 10644 src/main.rs 3e38a71ebbdf140392c28bd1e065bd2b9dd9588c
-        let parts:Vec<&str> = raw_entry.split(" ").collect();
+impl From<String> for IndexEntry{
+    fn from(value: String) -> Self {
+        let parts:Vec<&str> = value.split(" ").collect();
         let mut iter = parts.iter();
         let mode = iter.next().unwrap();
         let file_path = iter.next().unwrap();
@@ -22,5 +24,11 @@ impl IndexEntry{
         let mode = Mode::from(*mode);
 
         Self { mode, file_path: file_path.to_string(), hash: hash.to_string() }
+    }
+}
+
+impl From<IndexEntry> for String{
+    fn from(value: IndexEntry) -> Self {
+        format!("{} {} {}",value.mode,value.file_path,value.hash)
     }
 }
