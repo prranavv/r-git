f704e6442b99867301e1e5bf6d55749c9aeaf677
prranavv <pranavvkumar03@gmail.com>
2026-04-27 12:19:43 +0530

    fixed bug in update_index_contents

--- src/internal/utils/update_index_contents.rs
+++ src/internal/utils/update_index_contents.rs
@@ -10,14 +10,15 @@ pub fn update_index_contents(index_contents:String,index_entry:String)->Result<(
     let mut result = String::new();
     for line in lines{
         // println!("{}",line)
-        let mut line_entry = IndexEntry::from(line.to_string());
+        let line_entry = IndexEntry::from(line.to_string());
         if line_entry.file_path==new_entry.file_path{
-            line_entry.hash=new_entry.hash.clone();
+            continue;
         }
         let str_line_entry:String = line_entry.into();
         result.push_str(&str_line_entry);
         result.push_str("\n");
     }
+    result.push_str(&new_entry.to_string());
     let result =result.trim_end();
 
     let index_path = format!(".rgit/index");
