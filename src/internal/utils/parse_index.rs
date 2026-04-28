3529ac9b52cb1357eae91885af4089ae56d61aea
prranavv <pranavvkumar03@gmail.com>
2026-04-28 15:33:31 +0530

    added error handling to parse_index

--- src/internal/utils/parse_index.rs
+++ src/internal/utils/parse_index.rs
@@ -1,13 +1,14 @@
 use crate::internal::index_entry::IndexEntry;
 use std::fs;
+use crate::Result;
 
-pub fn parse_index()->Vec<IndexEntry>{
+pub fn parse_index()->Result<Vec<IndexEntry>>{
     let contents = fs::read_to_string(".rgit/index").unwrap();
     let lines = contents.lines();
     let mut result = Vec::new();
     for line in lines {
-        let index_entry = IndexEntry::from(line);
+        let index_entry = IndexEntry::try_from(line)?;
         result.push(index_entry);
     }
-    result
+    Ok(result)
 }
