69586859cb31dde006292fed70037efe39f4218b
prranavv <pranavvkumar03@gmail.com>
2026-04-29 18:10:27 +0530

    added comments to command flags

--- src/internal/enums/commands.rs
+++ src/internal/enums/commands.rs
@@ -12,16 +12,21 @@ pub enum Commands{
     /// Compute object ID and optionally create an object from a file
     HashObject{
         name:String,
+        /// write the object into the object database
         #[arg(short,long)]
         write:bool,
+        /// read the object from stdin
         #[arg(short,long)]
         stdin:bool
     },
     /// Provide contents or details of repository objects
     CatFile{
+        /// An existing object.
         hash:String,
+        /// Pretty-print the contents of <object> based on its type.
         #[arg(short='p')]
         pretty_print:bool,
+        /// Instead of the content, show the object type identified by <object>.
         #[arg(short)]
         type_of: bool
     },
@@ -29,11 +34,14 @@ pub enum Commands{
     WriteTree,
     /// List the contents of a tree object
     LsTree{
+        //. Id of a tree-ish.
         name:String
     },
     /// Create a new commit object
     CommitTree{
+        /// An existing tree object.
         tree_hash:String,
+        /// commit message
         #[arg(short,long)]
         message:String
     },
@@ -41,20 +49,25 @@ pub enum Commands{
     Log,
     /// Switch branches or restore working tree files
     Checkout{
+        /// Branch to checkout from or commit hash to checkout from.
         commit_hash:String
     },
     /// Create a new branch
     Branch{
+        /// The name of the branch to create
         branch_name: String
     },
     /// Add file contents to the index
     Add{
+        /// File to add to to the staging area
         file_name: PathBuf
     },
     /// Record changes to the repository
     Commit{
+        /// Use <msg> as the commit message. 
         #[arg(short='m')]
         message:String,
+        /// commit all changed files
         #[arg(short='a')]
         all:bool,
     },
@@ -62,7 +75,9 @@ pub enum Commands{
     Status,
     /// Remove files from the working tree and from the index
     Rm{
+        /// Files to remove. A leading directory name (e.g.  dir to remove dir/file1 and dir/file2)
         file_path:PathBuf,
+        /// Use this option to unstage and remove paths only from the index. Working tree files, whether modified or not, will be left alone.
         #[arg(long="cached")]
         cached:bool,
     }
