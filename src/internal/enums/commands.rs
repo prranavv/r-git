0293d4768702bc1a36d13124b83277f6f3a6fb77
prranavv <pranavvkumar03@gmail.com>
2026-04-29 16:10:36 +0530

    added comments for Commands

--- src/internal/enums/commands.rs
+++ src/internal/enums/commands.rs
@@ -3,9 +3,11 @@ use std::path::PathBuf;
 use clap::{Subcommand};
 
 #[derive(Subcommand)]
-#[command(version, about, long_about = None)]
+#[command(version="1.0.0", about, long_about = "Rust Implementation of Git")]
 pub enum Commands{
+    /// Create an empty Git repository or reinitialize an existing one
     Init,
+    /// Compute object ID and optionally create an object from a file
     HashObject{
         name:String,
         #[arg(short,long)]
@@ -13,6 +15,7 @@ pub enum Commands{
         #[arg(short,long)]
         stdin:bool
     },
+    /// Provide contents or details of repository objects
     CatFile{
         hash:String,
         #[arg(short='p')]
@@ -20,32 +23,42 @@ pub enum Commands{
         #[arg(short)]
         type_of: bool
     },
+    /// Create a tree object from the current index
     WriteTree,
+    /// List the contents of a tree object
     LsTree{
         name:String
     },
+    /// Create a new commit object
     CommitTree{
         tree_hash:String,
         #[arg(short,long)]
         message:String
     },
+    /// Show commit logs
     Log,
+    /// Switch branches or restore working tree files
     Checkout{
         commit_hash:String
     },
+    /// Create a new branch
     Branch{
         branch_name: String
     },
+    /// Add file contents to the index
     Add{
         file_name: PathBuf
     },
+    /// Record changes to the repository
     Commit{
         #[arg(short='m')]
         message:String,
         #[arg(short='a')]
         all:bool,
     },
+    /// Show the working tree status
     Status,
+    /// Remove files from the working tree and from the index
     Rm{
         file_path:PathBuf,
         #[arg(long="cached")]
