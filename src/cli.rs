d1d78058744eaf9814315905609f9c2f3fec05d0
prranavv <pranavvkumar03@gmail.com>
2026-04-30 11:27:48 +0530

    made rust about bit more accurate

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use crate::internal::enums::Commands;
 #[derive(Parser)]
 #[command(name="rgit")]
 #[command(version="1.0.0")]
-#[command(about = "Rust Implementation of Git", long_about = None)]
+#[command(about = "Rust Implementation(parts of it) of Git", long_about = None)]
 pub struct Cli{
     #[command(subcommand)]
     pub command:Commands
