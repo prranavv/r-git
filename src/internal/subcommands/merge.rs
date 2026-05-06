e38fcdb82b736a130b38d197de7729819dd571ce
prranavv <pranavvkumar03@gmail.com>
2026-05-06 12:01:13 +0530

    Update merge.rs

--- src/internal/subcommands/merge.rs
+++ src/internal/subcommands/merge.rs
@@ -3,7 +3,7 @@ use crate::{Result, internal::utils::get_commits_for_branch};
 pub fn merge(branch_name:&String)->Result<()>{
     let commits = get_commits_for_branch(branch_name)?;
     for c in commits{
-        println!("{}",c.to_string());
+        println!("{}",c);
     }
     Ok(())
 }
